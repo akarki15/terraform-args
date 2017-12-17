34466296d76189e21bb980ee6d35f9b25bda136c
akarki15 <aashish@clypd.com>
2017-12-17 00:15:06 -0500

    wip

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -8,12 +8,22 @@ import (
 )
 
 func Test_parseDir(t *testing.T) {
-	_, _ = parseDir("/Users/aashishkarki/Desktop/terraform-provider-aws-master/website/docs/d")
+	_, _ = parseDir("/Users/aashishkarki/Desktop/terraform-provider-aws-master/website/docs/d", "d")
 }
 
 func Test_parseDir_full(t *testing.T) {
-	for _, dir := range dirs {
-		commands, err := parseDir(dir)
+	for _, dir := range dDirs {
+		commands, err := parseDir(dir, "d")
+		assert.NoError(t, err)
+		b, err := json.Marshal(commands)
+		assert.NoError(t, err)
+		t.Log(string(b))
+	}
+}
+
+func Test_parse_d_Dir(t *testing.T) {
+	for _, dir := range rDir {
+		commands, err := parseDir(dir, "r")
 		assert.NoError(t, err)
 		b, err := json.Marshal(commands)
 		assert.NoError(t, err)
